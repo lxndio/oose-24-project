8d159a8f3003d5e326340062d84df127260baaa8
Emanuel162 <EmanuelNierstenhoefer@web.de>
2024-07-07 14:52:27 +0200

    feat: sprint 3 c add CourseController

--- src/main/java/com/sse/ooseproject/repositories/CourseRepository.java
+++ src/main/java/com/sse/ooseproject/repositories/CourseRepository.java
@@ -1,7 +1,17 @@
 package com.sse.ooseproject.repositories;
 
 import com.sse.ooseproject.models.Course;
+import com.sse.ooseproject.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByOrderByIdAsc();
+    List<Course> findByOrderByNameAsc();
+    List<Course> findByOrderByChairAsc();
+    List<Course> findByOrderByIdDesc();
+    List<Course> findByOrderByNameDesc();
+    List<Course> findByOrderByChairDesc();
 }
